bf395b8bc7b30dd811268ad36a7729235966cc2c
Zaki <muhzakierbaisyas@gmail.com>
2024-03-25 21:25:22 +0700

    second commit

--- src/app/galeri/page.jsx
+++ src/app/galeri/page.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Galeri = () => {
   return (
     <div className="bg-emerald-50 min-h-screen py-12">
@@ -5,23 +7,23 @@ const Galeri = () => {
         <h1 className="text-3xl font-bold mb-8">Galeri</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           <div className="bg-white rounded-lg overflow-hidden">
-            <img src="/gambar1.png" alt="Gambar 1" className="w-full h-auto" />
+            <Image src="/gambar1.png" alt="Gambar 1" className="w-full h-auto" />
             <div className="p-4">
-              <p className="text-lg font-medium mb-2">"Tidak ada jalan pintas menuju ke mana pun yang bernilai pergi."</p>
+              <p className="text-lg font-medium mb-2">Tidak ada jalan pintas menuju ke mana pun yang bernilai pergi.</p>
               <p><br />- Beverly Sills</p>
             </div>
           </div>
           <div className="bg-white rounded-lg overflow-hidden">
-            <img src="/gambar2.png" alt="Gambar 2" className="w-full h-auto" />
+            <Image src="/gambar2.png" alt="Gambar 2" className="w-full h-auto" />
             <div className="p-4">
-              <p className="text-lg font-medium mb-2">"Keberanian tidak berarti tidak takut. Keberanian berarti bertindak meskipun takut."</p>
+              <p className="text-lg font-medium mb-2">Keberanian tidak berarti tidak takut. Keberanian berarti bertindak meskipun takut.</p>
               <p><br />- Nelson Mandela</p>
             </div>
           </div>
           <div className="bg-white rounded-lg overflow-hidden">
-            <img src="/gambar3.png" alt="Gambar 3" className="w-full h-auto" />
+            <Image src="/gambar3.png" alt="Gambar 3" className="w-full h-auto" />
             <div className="p-4">
-              <p className="text-lg font-medium mb-2">"Kesuksesan tidak final, kegagalan tidak fatal: yang penting adalah keberanian untuk terus maju."</p>
+              <p className="text-lg font-medium mb-2">Kesuksesan tidak final, kegagalan tidak fatal: yang penting adalah keberanian untuk terus maju.</p>
               <p><br />- Winston Churchil</p>
             </div>
           </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 const Beranda = () => {
   return (
     <div className="bg-emerald-50 min-h-screen flex justify-center items-center">
       <div className="bg-white shadow-md rounded-lg max-w-4xl mx-auto overflow-hidden md:flex items-center">
         <div className="md:w-1/3 flex justify-center mb-4 md:mb-0">
-          <img src="/profile.png" alt="Zaki's Photo" className="w-48 h-48 rounded-full object-cover" />
+          <Image src="/profile.png" alt="Zaki's Photo" className="w-48 h-48 rounded-full object-cover" />
         </div>
         <div className="md:w-2/3 px-4 py-4">
           <p className="text-lg text-emerald-950 mt-4 mb-4 leading-relaxed">Sebagai seorang mahasiswa yang berfokus pada ilmu Teknologi Informasi, saya memiliki kecintaan yang mendalam terhadap dunia teknologi dan komputasi. Ketertarikan saya terhadap dunia IT tidak hanya sebatas pada penggunaan teknologi, tetapi juga pada pemahaman yang mendalam tentang fondasi dan konsep-konsep yang mendasarinya.</p>
